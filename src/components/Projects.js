3e61766393ce759b0eaeb44c9caae2557637f412
elizchan <lizchan@bu.edu>
2021-01-30 18:31:58 -0500

    added urls for project sites and github

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,12 +13,12 @@ const Projects = () => {
                         <div class="card-image">
                             <img src=""></img>
                             <div class="card-content black-text">
-                                <span class="card-title"><strong>K-Trivia</strong></span>
+                                <span class="card-title"><strong>K-Culture-Trivia</strong></span>
                                 <p>a Korean culture trivia game that is played between 2 people. It was built using Vanilla JS, CSS and HTML</p>
                             </div>
                             <div class="card-action">
-                                <a href="#">Link to Trivia</a>
-                                <a href="#">Link to Code</a>
+                                <a href="https://elizchan.github.io/K-Culture-Trivia/">Link to Trivia</a>
+                                <a href="https://github.com/elizchan/K-Culture-Trivia">Link to Code</a>
                             </div>
                         </div>
                     </div>
@@ -28,12 +28,12 @@ const Projects = () => {
                         <div class="card-image">
                             <img src=""></img>
                             <div class="card-content black-text">
-                                <span class="card-title"><strong>Smash Directory</strong></span>
+                                <span class="card-title"><strong>SuperSmash Directory</strong></span>
                                 <p>a website built by a team using Node.js, SQL, CSS uses a Yelp API to generate a list of nearby cafes to the user and allow the users to leave their own comments under the cafe page.</p>
                             </div>
                             <div class="card-action">
-                                <a href="#">Link to Site</a>
-                                <a href="#">Link to Code</a>
+                                <a href="https://supersmash-directory.herokuapp.com/">Link to Site</a>
+                                <a href="https://github.com/elizchan/SuperSmash-Directory">Link to Code</a>
                             </div>
                         </div>
                     </div>
@@ -41,14 +41,14 @@ const Projects = () => {
                 <div class="row">
                     <div class="col s12 m6">
                         <div class="card-image">
-                            <img src=""></img>
+                            <img src="https://i.imgur.com/qgxEYGw.png" width="399"></img>
                             <div class="card-content black-text">
                                 <span class="card-title"><strong>Roast</strong></span>
                                 <p>a website/app built using React.js, Mongodb, Mongoose,and CSS takes uses a SuperSmash Directory and allows users to make an account, add favorite SuperSmash characters to their profile and leave comments on a message board.</p>
                             </div>
                             <div class="card-action">
                                 <a href="http://roast-cafes.surge.sh/">Link to Site</a>
-                                <a href="#">Link to Code</a>
+                                <a href="https://github.com/elizchan/Roast-Front">Link to Code</a>
                             </div>
                         </div>
                     </div>
