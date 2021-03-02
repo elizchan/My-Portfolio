c8509805c6c7d8798dabde415d05f0ca043d32f2
elizchan <lizchan@bu.edu>
2021-03-02 18:46:57 -0500

    fixed typo in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <img className="profile-img-card" src="https://i.ibb.co/NtFgbFY/FB-IMG-1608238633178.jpg" alt="FB-IMG-1608238633178" border="0" />
                 <div className="card-body">
                     <h2 className="card-title">About Me!</h2>
-                    <p className="card-body">I am a Software engineer who is constantly learning. I attribute my success to being hardworking at any task I am given, reliable in getting the job done, and honest communication with me team. These characteristics compose the foundations of my working style, and I am confident in my fluency in multiple frameworks and programming languages to succeed as a developer.</p>
+                    <p className="card-body">I am a Software engineer who is constantly learning. I attribute my success to being hardworking at any task I am given, reliable in getting the job done, and honest communication with my team. These characteristics compose the foundations of my working style, and I am confident in my fluency in multiple frameworks and programming languages to succeed as a developer.</p>
                 </div>
             </div>
         </div>
