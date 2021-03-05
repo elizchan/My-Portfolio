8c9e85e88b8bb3012c6f48ce19c344294a27e71a
elizchan <lizchan@bu.edu>
2021-03-05 11:59:33 -0500

    updated email address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     return(
         <div>
             <h3>Let's collaborate!</h3>
-            <p>E-mail: lizchan@bu.edu</p>
+            <p>E-mail: liz.chandra@gmail.com</p>
         </div>
     )
 }
